ba963bba49259b9fb44989a3d751779fdf176546
Gonzalo Gatius <128533339+GonzaloGatius@users.noreply.github.com>
2024-03-27 12:21:45 -0300

    SqlDB

---
+++ libraryhue/DB/ConnectionStringData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryhue.DB
+{
+    public class ConnectionStringData
+    {
+        public string ConnectionStringName { get; set; }
+
+        public ConnectionStringData()
+        {
+            ConnectionStringName = "Default";
+        }
+    }
+}
---
+++ libraryhue/DB/SqlDB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace libraryhue.DB
+{
+    public class SqlDB
+    {
+        private readonly IConfiguration configuration;
+
+        public SqlDB(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            var connectionString = configuration.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+
+                var rows = await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+                
+                return rows.ToList();
+            }
+        }
+
+        public async Task<int> SaveData<U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            var connectionString = configuration.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+
+                return await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+
+            }
+        }
+    }
+}
