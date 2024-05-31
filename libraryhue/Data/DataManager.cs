3a757d90613c34e8a6a3ce0b2ae74c756bca66d0
Gonzalo Gatius <128533339+GonzaloGatius@users.noreply.github.com>
2024-05-31 18:35:34 -0300

    Datas

---
+++ libraryhue/Data/DataManager.cs
@@ -0,0 +1,47 @@
+﻿using libraryhue.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryhue.Data
+{
+    public abstract class DataManager
+    {
+        private readonly IDataAccess dataAccess;
+        private readonly ConnectionStringData connectionStringData;
+
+        protected abstract string tableName { get; set; }
+
+        public DataManager(IDataAccess dataAccess, ConnectionStringData connectionStringData)
+        {
+            this.dataAccess = dataAccess;
+            this.connectionStringData = connectionStringData;
+        }
+
+        public async Task<List<T>> GetAll<T>() where T : class
+        {
+            return await dataAccess.LoadData<T, dynamic>("spGenerics_getAll", new { @tableName = tableName }, connectionStringData.ConnectionStringName);
+
+        }
+
+        public async Task<T> GetById<T>(int Id) where T : class
+        {
+            var list = await dataAccess.LoadData<T, dynamic>("spGenerics_getById", new { @Id = Id, @tableName = tableName }, connectionStringData.ConnectionStringName);
+
+            return list.FirstOrDefault();
+        }
+
+        public async Task Delete(int Id)
+        {
+            await dataAccess.SaveData<dynamic>("spGenerics_delete", new { @Id = Id, @tableName = tableName }, connectionStringData.ConnectionStringName);
+
+        }
+        public async Task CreateIntegers(int newInt)
+        {
+            await dataAccess.SaveData<dynamic>("spGenerics_createIntegers", new {@newInt = newInt, @tableName = tableName }, connectionStringData.ConnectionStringName);
+
+        }
+    }
+}
---
+++ libraryhue/Data/UserData.cs
@@ -0,0 +1,24 @@
+﻿using libraryhue.DB;
+using libraryhue.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryhue.Data
+{
+    public class UserData
+    {
+        private readonly IDataAccess dataAccess;
+        private readonly ConnectionStringData connectionStringData;
+
+        public UserData(IDataAccess dataAccess, ConnectionStringData connectionStringData)
+        {
+            this.dataAccess = dataAccess;
+            this.connectionStringData = connectionStringData;
+        }
+
+        
+    }
+}
